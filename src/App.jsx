f7a31ec3249928eb95e4e7c776149e7381581142
oizzoo <108180322+oizzoo@users.noreply.github.com>
2026-04-09 14:42:18 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSidebar from "./components/ProjectSidebar";
+
+function App() {
+  return (
+    <>
+      <ProjectSidebar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,8 @@
+export default function ProjectSidebar() {
+  return (
+    <div>
+      <h1>Your Projects</h1>
+      <button>+ Add Project</button>
+    </div>
+  );
+}
